f892de041817f0615511a4d97cec49e232509157
jhon.sangopanta@epn.edu.ec <jhon.sangopanta@epn.edu.ec>
2022-09-20 12:38:22 -0500

    Dia 2

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import "../estilos/Boton.css"
 
-const esOperador = (valor) => {
-    return isNaN(valor) && (valor != ".") && (valor != "=")
-};
+
+
+
 
 const Boton = (props) => {
-    return (
-        <div
-            className={`boton-contenedor ${esOperador(props.children) ? "operador":"".trimEnd()}`}
-            onClick={() => props.manejarClick(props.children)}>  {/* Siempre se debe hacer un llamado a una funcion, no a su valor */}
-            {props.children}
-        </div>
-    );
+
+    const esOperador = (valor) => {
+        return isNaN(valor) && (valor !== ".") && (valor !== "=")
+    };
+
+    
+    if(esOperador(props.children)){
+        return (
+            <div
+                className="boton-contenedor operador"
+                onClick={() => props.manejarClick(props.children)}>  {/* Siempre se debe hacer un llamado a una funcion, no a su valor */}
+                {props.children}
+            </div>
+        );
+    }else{
+        return (
+            <div
+                className="boton-contenedor"
+                onClick={() => props.manejarClick(props.children)}>  {/* Siempre se debe hacer un llamado a una funcion, no a su valor */}
+                {props.children}
+            </div>
+        );
+    }
 }
 
 export default Boton;
